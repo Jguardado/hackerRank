df47e5951709caa2bbdc20ae677cde857f733351
Juan <jguardado85@gmail.com>
2017-07-24 13:17:28 -0700

    close

--- codeWars/rot13.js
+++ codeWars/rot13.js
@@ -11,7 +11,8 @@ const rot13 = (message) => {
     's', 't', 'u',
     'v', 'w', ' x',
     'y', 'z'];
-  const encrypted = [];
+  let encrypted = [];
+  const capitalLetterIndex = {};
   const alphaIndex = (currentIndex) => {
     const newIndex = currentIndex + 13;
     if (newIndex <= 26) {
@@ -20,15 +21,28 @@ const rot13 = (message) => {
     return newIndex - 26;
   };
 
-  splitWord.forEach((letter) => {
+  splitWord.forEach((letter, letterIndex) => {
     letterArr.forEach((possibleMatch, i) => {
       if (letter === possibleMatch) {
         encrypted.push(letterArr[alphaIndex(i)]);
+      } else if (letter.toLowerCase() === possibleMatch) {
+        capitalLetterIndex[letterIndex] = true;
       }
     });
   });
+
+  if (Object.keys(capitalLetterIndex)) {
+    encrypted = encrypted.map((letter, i) => {
+      if (capitalLetterIndex[i]) {
+        console.log('going trhough capp', i);
+        return letter.toUpperCase();
+      }
+      return letter;
+    });
+  }
+  console.log('capitalLetterIndex: ', capitalLetterIndex);
   console.log('encrypted: ', encrypted.join(''));
   return encrypted;
 };
 
-rot13('test');
+rot13('Test');
